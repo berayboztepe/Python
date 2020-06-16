3e9e25f72d653d98dc85392172ca23aa27e2a8ab
Beray Boztepe <44292203+berayboztepe@users.noreply.github.com>
2020-06-16 14:21:54 +0300

    Add files via upload

---
+++ Coursera_Python/Data_Structures/coursera-finding-and-prompting-e-mails.py
@@ -0,0 +1,19 @@
+#reading a file, finding all of the e-mails
+#and prompting them and counts
+
+
+fname = input("Enter file name: ")
+if len(fname) < 1: fname = "names.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+    if not line.startswith('From '):
+        continue
+
+    line1 = line.split()
+    count += 1
+    print(line1[1])
+
+print("There were", count, "lines in the file with From as the first word")
