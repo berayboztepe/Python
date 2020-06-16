1cd103517ef7055c4c5171c68f724601f16437e9
Beray Boztepe <44292203+berayboztepe@users.noreply.github.com>
2020-06-17 01:42:19 +0300

    Add files via upload

---
+++ Coursera_Python/Web_Data/coursera-geography-JSON.py
@@ -0,0 +1,49 @@
+#by the datas of google api, this code gives the infos of a location that u've printing
+#street, city, country, post code etc...
+#and it also gives latitude and longitude of the places
+#also after u enter a place name, code will create an URL which has the place name in it.
+#if u click it, u will see the results better in that website
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+api_key = False
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+    try:
+        js = json.loads(data.decode())
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
